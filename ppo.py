6c27194569b88c31dafcab66d8b752c3a6e24e31
Pranav-India <pranavsk08@gmail.com>
2021-04-15 16:31:45 +0530

    ppo created

--- ppo.py
+++ ppo.py
@@ -1,2 +1,16 @@
+import torch
+from torch.distributions import MultivariateNormal
+
+from network import FeedForwardNN
+
+
 class ppo:
+    def __init__(self, env):
+        # Extract environment information
+        self.env = env
+        self.obs_dim = env.observation_space.shape[0]
+        self.act_dim = env.action_space.shape[0]
+        self.actor = FeedForwardNN(self.obs_dim, self.act_dim)
+        self.critic = FeedForwardNN(self.obs_dim, 1)
+
     def learn(self):
